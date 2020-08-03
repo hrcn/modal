9fb838989291679ad4aee056ea135fa43223647d
Rui Han <hrcn1128@gmail.com>
2020-08-03 01:38:47 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Modal from './modal/Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Modal />
+      </div>
+    )
+  }
+}
+
+export default App;
